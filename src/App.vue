17b4e25d3a364b8e11ad9db034313495b00b27c7
= <nglomski@gmail.com>
2017-12-19 10:59:05 -0600

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <div><button @click="toggleLogo">Toggle Logo</button></div>
+    <img src="./assets/logo.png" v-if="showLogo">
+    <h1>{{ msg }}</h1>
+    <vfhHelloWorld></vfhHelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './HelloWorld.vue';
+
+export default {
+  name: 'app',
+  components: {
+    vfhHelloWorld: HelloWorld
+  },
+  data () {
+    return {
+      msg: 'Your Vue.js App',
+      showLogo: true
+    }
+  },
+  methods: {
+    toggleLogo() {
+      this.showLogo = !this.showLogo;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  h1 {
+    font-weight: normal;
+    font-size: 48px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="vfhHelloWorld">
+    <h1>Hello {{target}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        target: "World"
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .vfhHelloWorld {
+    h1 {
+      font-size: 36px;
+    }
+  }
+</style>
